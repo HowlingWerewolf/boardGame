82290673b691061a769761deab546d7fc4f2d9b3
Attila Gavaller <backdoor.werewolf@gmail.com>
2016-08-02 00:07:05 +0200

    sonar major bug fix (comment explaining why this method is empty)

--- src/main/java/com/mycompany/view/HelloWorldApplication.java
+++ src/main/java/com/mycompany/view/HelloWorldApplication.java
@@ -3,7 +3,9 @@ package com.mycompany.view;
 import org.apache.wicket.protocol.http.WebApplication;
 
 public class HelloWorldApplication extends WebApplication {
+	
     public HelloWorldApplication() {
+    	// Do nothing
     }
 
     @Override
