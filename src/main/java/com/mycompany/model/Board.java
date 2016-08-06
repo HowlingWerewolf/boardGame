a4fd04ecf6b224a3b60df70aa5bd715dcfa0ddff
Attila Gavaller <backdoor.werewolf@gmail.com>
2016-08-06 19:34:44 +0200

    added some comments to Board abstract

--- src/main/java/com/mycompany/model/Board.java
+++ src/main/java/com/mycompany/model/Board.java
@@ -1,5 +1,14 @@
 package com.mycompany.model;
 
+/**
+ * 
+ * @author Gavi
+ * I didn't define anything there, since every game has different entityset. 
+ * Like not all games depend on cards, or dices, and so on...
+ * 
+ * It's implementation, that defines these things!
+ *
+ */
 public abstract class Board {
 
 }
